994d61386910b7e2eaff9b5eea6ed5d94c0ec35c
CubePhoenix <lbu01@buchli-bigger.ch>
2018-07-05 14:23:24 +0200

    Added initial game files

---
+++ gamestates/Planet1.gd
@@ -0,0 +1,20 @@
+extends StaticBody2D
+
+const SCALE_DIVISOR = 16500.0
+const COLL_DIVISOR = 18.2
+
+export var mass = 500.0
+export var is_finish = false
+
+func _ready():
+	if is_finish:
+		get_node("Sprite").set_texture(load("res://assets/planets/finish.png"))
+	else:
+		get_node("Sprite").set_texture(load("res://assets/planets/1.png"))
+	
+	# Set to size corresponding to mass
+	get_node("Sprite").set_scale(Vector2(mass / SCALE_DIVISOR, mass / SCALE_DIVISOR))
+	
+	var shape = CircleShape2D.new()
+	shape.set_radius(mass / COLL_DIVISOR)
+	get_node("CollisionShape").set_shape(shape)
---
+++ gamestates/Player.gd
@@ -0,0 +1,138 @@
+extends KinematicBody2D
+
+const SENSITIVITY_DIVISOR = 20
+const GRAVITY_MULTIPLIER = 0.002
+
+enum gamestates {
+	FINISHED_SUCCESS, FINISHED_FAILURE, FLYING, AWAIT_INPUT
+}
+
+# current gamestate of the game
+var state
+var direction
+var original_position
+var finished
+
+func _ready():
+	# initialization of global variables
+	state = gamestates.AWAIT_INPUT
+	direction = Vector2(0, 0)
+	original_position = position
+	finished = 0.0
+
+func _physics_process(delta):
+	# call sub-process function depending on gamestate
+	if state == gamestates.AWAIT_INPUT:
+		process_await_input(delta)
+	elif state == gamestates.FLYING:
+		process_flying(delta)
+	else:
+		process_finished(delta)
+
+
+func process_await_input(delta):
+	if (Input.is_mouse_button_pressed(BUTTON_LEFT)): # Touch input is registered as left mouse button
+		# get touch location relative to player satellite position
+		# TODO make thrust velocity non-linear
+		direction = -(get_position() - get_viewport().get_mouse_position()) / SENSITIVITY_DIVISOR
+		
+		state = gamestates.FLYING
+		
+		# Thrust animation
+		get_node("Particles/Thruster1").set_emitting(true)
+		get_node("Particles/Thruster2").set_emitting(true)
+		get_node("Timers/ThrustTimer").start()
+		yield(get_node("Timers/ThrustTimer"), "timeout")
+		get_node("Particles/Thruster1").set_emitting(false)
+		get_node("Particles/Thruster2").set_emitting(false)
+	else:
+		# Wait for next round
+		pass
+	
+func process_flying(delta):
+	# fly further in direction (adjust according to gravitation)
+	# and finish if satellite hit planet (win for right planet, lose for wrong)
+	# restart level if there is user input
+
+	for planet in get_parent().get_node("Planets").get_children():
+		direction += get_gravitational_force(planet)
+	
+	var col = move_and_collide(direction)
+	
+	# Rotate hitbox and texture
+	rotate_to(direction)
+	
+	# if a collision occured
+	if col:
+		if col.get_collider().get("is_finish"):
+			state = gamestates.FINISHED_SUCCESS
+		else:
+			state = gamestates.FINISHED_FAILURE
+	
+	if (Input.is_mouse_button_pressed(BUTTON_LEFT)):
+		reset_level()
+
+func process_finished(delta):
+	# display end screen 
+	if finished == 0.0: # if not set yet
+		finished = delta
+		
+		if state == gamestates.FINISHED_SUCCESS:
+			get_parent().get_node("HUD/LevelSuccess").popup(Rect2(100, 100, 400, 400))
+		else:
+			# Play the explosion and reset
+			get_node("Timers/ResetTimer").start()
+			get_node("Sprite").hide()
+			get_node("Particles/Explosion").set_emitting(true)
+			yield(get_node("Timers/ResetTimer"), "timeout")
+			get_node("Sprite").show()
+			get_node("Particles/Explosion").set_emitting(false)
+			reset_level()
+	
+	# TODO buttons on end screen instead of just clicking
+	if (Input.is_mouse_button_pressed(BUTTON_LEFT)):
+		reset_level()
+
+func get_gravitational_force(planet):
+	# Get planets properties
+	var mass = planet.mass
+	var pos = get_node_position(planet)
+	var force = Vector2(0, 0)
+	
+	# Get relative position
+	var rel_pos = get_position() - pos
+	
+	# Check if we're in range of the gravitational force
+	if (rel_pos.x < mass) and (rel_pos.y < mass):
+		
+		# Calculate distance from planet
+		var distance = sqroot((rel_pos.x * rel_pos.x) + (rel_pos.y * rel_pos.y))
+
+		# Calculate force on the player
+		force = (Vector2(- sqroot(rel_pos.x), - sqroot(rel_pos.y)) / Vector2(distance, distance)) \
+			* Vector2(GRAVITY_MULTIPLIER, GRAVITY_MULTIPLIER) * Vector2(mass, mass)
+	return force
+
+
+func get_position():
+	return position
+
+func get_node_position(node):
+	return node.get("position")
+
+func reset_level():
+	state = gamestates.AWAIT_INPUT
+	position = original_position
+	finished = 0.0
+	direction = Vector2(0, 0)
+	
+	# TODO Spawn animation
+	
+func sqroot(fl):
+	if fl >= 0:
+		return sqrt(fl)
+	else:
+		return -sqrt(abs(fl))
+		
+func rotate_to(vec):
+	rotate(-atan2(-vec.x, -vec.y) - rotation)
